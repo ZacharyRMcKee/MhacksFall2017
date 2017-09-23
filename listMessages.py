c26fb446b55de7580a473658459dc745149c0761
ZacharyRMcKee <zmckee@hawk.iit.edu>
2017-09-23 16:04:01 -0400

    Updated listMessages module

--- listMessages.py
+++ listMessages.py
@@ -5,4 +5,5 @@ auth_token = "AA"
 client = Client(accout_sid,auth_token)
 
 for message in client.messages.list():
-    print(message.body)
+    if(message.body == "911"):
+        print("Calling the cops!")
